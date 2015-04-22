4400387488d07a2d36f55ac84f13245db2b8d5ea
wikisoft <wikicenter-soft@yahoo.fr>
2015-04-22 22:58:17 +0200

    Update demo-distributed-raidb1-controller1.bat

--- demo/demo-distributed-raidb1-controller1.bat
+++ demo/demo-distributed-raidb1-controller1.bat
@@ -16,14 +16,14 @@ goto end
 cd "%CJDBC_HOME%\bin"
 
 echo Starting hsqldb on port 9001
-start \B hsqldb.bat -port 9001
+start /B hsqldb.bat -port 9001
 
 echo Starting hsqldb on port 9002
-start \B hsqldb.bat -port 9002
+start /B hsqldb.bat -port 9002
 
 pause 
 
 echo Starting Controller 1 in distributed Configuration
-start \B controller.bat -f ..\config\controller\controller-distributed-1.xml
+start /B controller.bat -f ..\config\controller\controller-distributed-1.xml
 
 :end
